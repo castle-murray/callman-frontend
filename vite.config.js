18d94f42891d92d7140ff6c2f6447c675a3471d2
castlemurray <sean@castle-murray.com>
2026-02-08 18:27:13 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
       react(),
       tailwindcss(),
-  ],
+    ],
     server: {
-      allowedHosts: env.VITE_ALLOWED_HOSTS?.split(',') || []
-    }
+      allowedHosts: env.VITE_ALLOWED_HOSTS?.split(',') || [],
+    },
+  }
 })
